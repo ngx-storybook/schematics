4aad096e0c10e9336d3185d54037c1dbb9f7d49d
Christian Liebel <christian.liebel@thinktecture.com>
2019-07-29 15:44:30 +0200

    feat(project-setup): adjust storybook config on add

---
+++ src/project-setup/index.ts
@@ -0,0 +1,12 @@
+import { Rule, Tree } from '@angular-devkit/schematics';
+
+export function projectSetup(): Rule {
+  return (tree: Tree) => {
+    const configPath = '.storybook/config.js';
+    if (tree.exists(configPath)) {
+      let contents = tree.read(configPath) !.toString();
+      tree.overwrite(configPath, contents.replace("'../src/stories'", "'../src'"));
+    }
+    return tree;
+  };
+}
