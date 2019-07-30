9f09817dbab267dda1adab5c164e384bbc8eba68
Christian Liebel <christian.liebel@thinktecture.com>
2019-07-30 16:34:24 +0200

    feat(component): take component styles into account

--- src/component/index.ts
+++ src/component/index.ts
@@ -9,13 +9,25 @@ import {
     Rule, Tree,
     url
 } from '@angular-devkit/schematics';
+import { Style } from '@schematics/angular/component/schema';
 import { parseName } from '@schematics/angular/utility/parse-name';
 import { buildDefaultPath, getProject } from '@schematics/angular/utility/project';
 
+const configKey = '@schematics/angular:component';
+
 export function component(_options: any): Rule {
     return chain([
+        (host: Tree) => {
+            const project = getProject(host, _options.project);
+            const style = project.schematics && project.schematics[configKey] && project.schematics[configKey].style;
+            _options.style = style || Style.Css;
+        },
         externalSchematic('@schematics/angular', 'module', {name: _options.name}),
-        externalSchematic('@schematics/angular', 'component', {name: _options.name, module: _options.name}),
+        externalSchematic('@schematics/angular', 'component', {
+            name: _options.name,
+            module: _options.name,
+            get style() { return _options.style }
+        }),
         (host: Tree) => {
             const project = getProject(host, _options.project);
             const parsedPath = parseName(buildDefaultPath(project), _options.name);
